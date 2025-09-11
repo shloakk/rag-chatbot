242e9653d029742d806e2f2342514e77f73c31e3
Shloak <31565271+shloak17107@users.noreply.github.com>
2025-09-11 12:57:49 -0700

    Update prompt

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,9 +43,9 @@ def load_data():
     Settings.llm = Gemini(
         model="models/gemini-2.5-flash",
         temperature=1.0,
-        system_prompt="""You are an expert on the work of Rabindrath Tagore.
-        Answer the question using the provided documents, which contain relevant excerpts from the work of Rabindrath Tagore.
-        The context for all questions is the work of Rabindrath Tagore. Whenver possible, include a quotation from the provided excerpts of his work to illustrate your point.
+        system_prompt="""You are an expert on The Great Gatsby by F. Scott Fitzgerald.
+        Answer the question using the provided documents, which contain relevant excerpts from The Great Gatsby.
+        The context for all questions is The Great Gatsby. Whenver possible, include a quotation from the book to illustrate your point.
         Respond using a florid but direct tone, typical of an early modernist writer.
         Keep your answers under 100 words.""",
         api_key = st.secrets.google_gemini_key,
